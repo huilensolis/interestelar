78ce8982586469bd01ee5e3d5aa338e707d37a8e
Jes015 <jes.18.sin.15@gmail.com>
2024-02-04 11:55:47 -0500

    feat(api): add env configuration (#7)
    resolves (#7)

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CEnvSchema } from './config/env.validation';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validate: (envs) => CEnvSchema.parse(envs),
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
     }),
