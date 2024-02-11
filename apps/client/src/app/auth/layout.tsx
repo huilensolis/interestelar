b6521bf6a9c2d889791dec6f35f5b65a0ce6d189
Huilensolis <xywvanz@gmail.com>
2024-02-11 13:08:29 -0300

    feat(app): 16 change auth layout image

--- apps/client/src/app/auth/layout.tsx
+++ apps/client/src/app/auth/layout.tsx
@@ -28,14 +28,26 @@ export default function AuthLayout({
 
 function Aside(): React.JSX.Element {
   return (
-    <article className="h-full w-full">
+    <article className="h-full w-full relative">
       <img
-        src={'/6.gradients.ray.st.png'}
+        src="https://cdn.dribbble.com/userupload/12793873/file/original-5cc23fe9b782d345c7009c851edf3652.png?resize=1024x768"
         width={640}
         height={900}
         alt="dark gradient"
         className="w-full h-full object-cover object-center"
       />
+      <span className="absolute bottom-0 right-0 p-4 backdrop-blur">
+        image by{' '}
+        <a
+          className="text-blue-500 hover:underline"
+          href="https://dribbble.com/shots/23553375-CTASK-Project-Task-Management-Dashboard"
+        >
+          F. Ferina Chung
+        </a>
+        <br />
+        This image will be replaced with a real one as the app development
+        advances
+      </span>
     </article>
   )
 }
