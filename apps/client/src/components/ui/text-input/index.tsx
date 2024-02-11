3d7d27eaf402667ac0e194072c86059a95955a62
Huilensolis <xywvanz@gmail.com>
2024-02-11 13:37:30 -0300

    refactor(app): 16 fix outline on text input

--- apps/client/src/components/ui/text-input/index.tsx
+++ apps/client/src/components/ui/text-input/index.tsx
@@ -11,7 +11,7 @@ export const TextInput = forwardRef<HTMLInputElement, TTextInputProps>(
       <input
         ref={ref}
         {...props}
-        className={`w-full p-2 rounded-md font-medium text-neutral-700 placeholder:text-neutral-400 placeholder:font-medium transition-colors duration-75 ${!isDirty && 'border border-neutral-300 focus:outline-2 focus:outline-blue-300'} ${isDirty && hasError && 'outline outline-2 outline-red-300'} ${isDirty && !hasError && 'outline outline-2 outline-green-300'}`}
+        className={`w-full p-2 rounded-md font-medium text-neutral-700 placeholder:text-neutral-400 placeholder:font-medium transition-colors duration-75 border ${!isDirty ? 'border-neutral-300 focus:outline focus:outline-2 focus:outline-blue-300' : 'border-transparent'} ${isDirty && hasError && 'outline outline-2 outline-red-300'} ${isDirty && !hasError && 'outline outline-2 outline-green-300'}`}
       />
     )
   }
