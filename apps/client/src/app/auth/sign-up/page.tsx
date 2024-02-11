331f6a53880916c1f317ed9c6f7999ac017e71f9
Huilensolis <xywvanz@gmail.com>
2024-02-10 22:14:16 -0300

    feat(app): 16 add text input component and start sign up form

--- apps/client/src/app/auth/sign-up/page.tsx
+++ apps/client/src/app/auth/sign-up/page.tsx
@@ -1,3 +1,9 @@
+import { SignUpForm } from './components/form'
+
 export default function SignUpPage(): React.JSX.Element {
-  return <>sign up</>
+  return (
+    <article className="w-full flex flex-col justify-center items-center px-5">
+      <SignUpForm />
+    </article>
+  )
 }
