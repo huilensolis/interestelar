5744999ac6295d6222525a13959624cba17461c7
Huilensolis <xywvanz@gmail.com>
2024-03-04 15:09:23 -0300

    refactor(client): 76 change spinner loader size

--- apps/client/src/components/ui/button/base-button.component.tsx
+++ apps/client/src/components/ui/button/base-button.component.tsx
@@ -27,7 +27,7 @@ function LoaderSpinner() {
     <div role="status">
       <svg
         aria-hidden="true"
-        className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+        className="w-6 h-6 text-neutral-50 animate-spin fill-blue-600"
         viewBox="0 0 100 101"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
