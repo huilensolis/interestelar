f50170eef9dafda8c36411085f789722c9161d03
Huilensolis <xywvanz@gmail.com>
2024-04-04 22:24:48 -0300

    refactor(client): 97 nav links ui

--- apps/client/src/components/features/nav-link/nav-link.component.tsx
+++ apps/client/src/components/features/nav-link/nav-link.component.tsx
@@ -25,7 +25,7 @@ export function NavLink({
       href={href}
       className={[
         className,
-        `w-full flex gap-2 items-center p-1 outline outline-2 outline-transparent focus:outline-blue-300 focus-visible:outline-blue-300 rounded-sm transition-colors duration-75 ${isActive ? 'text-neutral-800 font-semibold bg-gray-200' : 'text-neutral-500 font-medium hover:text-neutral-700 hover:bg-gray-200'}`,
+        `w-full flex gap-2 items-center p-1 outline outline-2 outline-transparent focus:outline-blue-300 focus-visible:outline-blue-300 rounded-sm transition-colors duration-75 text-neutral-800 ${isActive ? 'font-semibold bg-gray-200' : 'font-normal hover:text-neutral-700 hover:bg-gray-200'}`,
       ].join(' ')}
       {...props}
     >
